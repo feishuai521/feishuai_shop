ed0745a419c3d36cd69b5c7febe011ec07245bdb
feishuai <177187630@qq.com>
2022-04-05 16:06:37 +0800

    简化封装api接口还有增加参数修改删除功能

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 /**
  * @Author: 飞帅
  * @Date: 2022-03-29 15:33:29
- * @LastEditTime: 2022-04-04 09:55:00
+ * @LastEditTime: 2022-04-05 15:29:15
  * @LastEditors: feishuai
  * @Description: blog.feishuai521.cn`
  * @The copyright belongs to Fei Shuai
@@ -10,11 +10,16 @@
 import axios from './post'
 //判断失败与成功
 // Object.keys(arr).length == 1 ? this.$message.error(arr.msg) : this.$message.success(arr.msg)
-
-// Vue.use(ElementUI)\
 //获取数据展现给return
 export function sjzs(res, dk) {
-  console.log(res, dk)
+  if (res.meta.status !== dk)
+    return {
+      msg: res.meta.msg,
+    }
+  return {
+    msg: res.meta.msg,
+    data: res.data,
+  }
 }
 export async function roles() {
   const { data: res } = await axios({
@@ -29,199 +34,94 @@ export async function removeid(a, b) {
 }
 //
 export async function allocation() {
-  // console.log(falg)
   const { data: res } = await axios.get(`rights/tree`)
-  if (res.meta.status !== 200)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 200)
 }
 export async function acyiiis(id, dtas) {
-  console.log(id, dtas)
   const { data: res } = await axios.post(`roles/${id}/rights`, { rids: dtas })
-  if (res.meta.status !== 200)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 200)
 }
 export async function rolesls() {
   const { data: res } = await axios.get('roles')
-  if (res.meta.status !== 200)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 200)
 }
 export async function roleslsuser(id, dtas) {
-  console.log(id, dtas)
   const { data: res } = await axios.put(`users/${id}/role`, { rid: dtas })
-  if (res.meta.status !== 200)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 200)
 }
 export async function catelist(dtas) {
-  // console.log(dtas)
   const { data: res } = await axios.get('categories', dtas)
-  if (res.meta.status !== 200)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 200)
 }
 // categories
 export async function categorieslist(dtas) {
   // console.log(dtas)
   const { data: res } = await axios.get('categories', dtas)
-  if (res.meta.status !== 200)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 200)
 }
 export async function categoriesadd(dtas) {
   // console.log(dtas)
 
   const { data: res } = await axios.post('categories', dtas)
   // console.log(res)
-  if (res.meta.status !== 201)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 201)
 }
 export async function categoriesacc(id, dtas) {
-  console.log(dtas)
-
   const { data: res } = await axios.get(`categories/${id}/attributes`, dtas)
-  // console.log(res)
-  if (res.meta.status !== 200)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 200)
 }
 export async function categoriesadds(id, dtas) {
   const { data: res } = await axios.post(`categories/${id}/attributes`, dtas)
-  if (res.meta.status !== 201)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 201)
 }
 //添加角色用户
 export async function adduser(dtas) {
   const { data: res } = await axios.post('roles', dtas)
-  if (res.meta.status !== 201)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 201)
 }
 //获取角色用户id下的信息
 export async function getuser(id) {
   const { data: res } = await axios.get(`roles/${id}`)
-  if (res.meta.status !== 200)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 200)
 }
 //提交修改角色用户信息
 export async function putuser(id, dtas) {
-  console.log(id, dtas)
+  // console.log(id, dtas)
   const { data: res } = await axios.put(`roles/${id}`, dtas)
-  if (res.meta.status !== 200)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 200)
 }
 //删除提交修改角色用户信息
 export async function deluser(id) {
   const { data: res } = await axios.delete(`roles/${id}`)
-  if (res.meta.status !== 200)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 200)
 }
 //根据商品分类id获取商品分类信息
 export async function getcate(id) {
   const { data: res } = await axios.get(`categories/${id}`)
-  if (res.meta.status !== 200)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 200)
 }
-
 //修改商品分类信息
 export async function putcate(id, dtas) {
   const { data: res } = await axios.put(`categories/${id}`, dtas)
-  if (res.meta.status !== 200)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 200)
 }
 //删除商品分类信息
 export async function delcates(id) {
   const { data: res } = await axios.delete(`categories/${id}`)
-  if (res.meta.status !== 200)
-    return {
-      msg: res.meta.msg,
-    }
-  return {
-    msg: res.meta.msg,
-    data: res.data,
-  }
+  return sjzs(res, 200)
+}
+//  修改商品分类信息  属性
+export async function putcates(id, attrId, dtas) {
+  const { data: res } = await axios.get(`categories/${id}/attributes/${attrId}`, dtas)
+  return sjzs(res, 200)
+}
+//修改商品分类信息  属性
+export async function putcatesadd(id, attrId, dtas) {
+  const { data: res } = await axios.put(`categories/${id}/attributes/${attrId}`, dtas)
+  return sjzs(res, 200)
+}
+//删除商品分类信息  属性
+export async function delcatesadd(id, attrId) {
+  const { data: res } = await axios.delete(`categories/${id}/attributes/${attrId}`)
+  return sjzs(res, 200)
 }
--- src/components/goods/params.vue
+++ src/components/goods/params.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: 飞帅
  * @Date: 2022-03-31 19:44:44
- * @LastEditTime: 2022-04-01 18:59:28
+ * @LastEditTime: 2022-04-05 16:02:51
  * @LastEditors: feishuai
  * @Description: blog.feishuai521.cn`
  * The copyright belongs to Fei Shuai
@@ -18,7 +18,6 @@
       <el-row style="margin: 20px 0">
         <el-col>
           <span>选择商品分类：</span>
-
           <el-cascader expand-trigger="hover" :options="parmlist" :props="parval" v-model="selectedopt" @change="handleChange">
           </el-cascader>
         </el-col>
@@ -27,13 +26,28 @@
         <el-tab-pane label="动态参数" name="many">
           <el-button type="primary" size="mini" :disabled="isbled" @click="dialogVisible = true">添加参数</el-button>
           <el-table :data="manytabledata" style="width: 100%; margin: 10px 0" border stripe>
-            <el-table-column type="expand" label="#"> </el-table-column>
+            <el-table-column type="expand" label="#">
+              <template slot-scope="scope"
+                ><el-tag closable v-for="(item, index) in scope.row.attr_vals" :key="index">{{ item }}</el-tag>
+                <el-input
+                  class="input-new-tag"
+                  v-if="inputVisible"
+                  v-model="inputValue"
+                  ref="saveTagInput"
+                  size="small"
+                  @keyup.enter.native="handleInputConfirm"
+                  @blur="handleInputConfirm"
+                >
+                </el-input>
+                <el-button v-else class="button-new-tag" size="small" @click="showInput(scope.row)">+ New Tag</el-button>
+              </template>
+            </el-table-column>
             <el-table-column type="index" label="ID"> </el-table-column>
             <el-table-column label="参数名称" prop="attr_name"> </el-table-column>
             <el-table-column label="操作">
-              <template>
-                <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
-                <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+              <template slot-scope="scope">
+                <el-button type="primary" icon="el-icon-edit" size="mini" @click="addpardd(scope.row)">编辑</el-button>
+                <el-button type="danger" icon="el-icon-delete" size="mini" @click="removtpar(scope.row)">删除</el-button>
               </template>
             </el-table-column>
           </el-table>
@@ -41,14 +55,18 @@
         <el-tab-pane label="静态属性" name="only">
           <el-button type="primary" size="mini" :disabled="isbled" @click="dialogVisible = true">添加属性</el-button>
           <el-table :data="omlytabdata" style="width: 100%; margin: 10px 0" border stripe>
-            <el-table-column type="expand" label="#"> </el-table-column>
+            <el-table-column type="expand" label="#">
+              <template slot-scope="scope"
+                ><el-tag closable v-for="(item, index) in scope.row.attr_vals" :key="index">{{ item }}</el-tag>
+              </template>
+            </el-table-column>
             <el-table-column type="index" label="ID"> </el-table-column>
             <el-table-column label="属性名称" prop="attr_name"> </el-table-column>
             <el-table-column label="操作">
-              <template>
-                <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+              <template slot-scope="scope">
+                <el-button type="primary" icon="el-icon-edit" size="mini" @click="addpardd(scope.row)">编辑</el-button>
 
-                <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+                <el-button type="danger" icon="el-icon-delete" size="mini" @click="removtpar(scope.row)">删除</el-button>
               </template>
             </el-table-column>
           </el-table>
@@ -66,11 +84,22 @@
         <el-button type="primary" @click="addparsms">确 定</el-button>
       </span>
     </el-dialog>
+    <el-dialog :title="'修改' + titletext" :visible.sync="xgparam" width="50%" @close="addcscxg">
+      <el-form ref="addparsxgRef" :model="addpresxg" :rules="addprssxgRu" label-width="100px">
+        <el-form-item :label="titletext + ':'" prop="attr_name">
+          <el-input v-model="addpresxg.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="xgparam = false">取 消</el-button>
+        <el-button type="primary" @click="addparsmsxg">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { categorieslist, categoriesacc, categoriesadds } from '../../api'
+import { categorieslist, categoriesacc, categoriesadds, putcates, putcatesadd, delcatesadd } from '../../api'
 
 export default {
   data() {
@@ -95,10 +124,31 @@ export default {
           { min: 3, max: 8, message: '长度在 3 到 8 个', trigger: 'blur' },
         ],
       },
+      //修改分类内容
+      xgparam: false,
+      //id 存储 分类id
+      xgdata: {
+        attr_id: '',
+        id: '',
+      },
+      addpresxg: {
+        attr_name: '',
+        attr_vals: '',
+        attr_sel: '',
+      },
+      addprssxgRu: {
+        attr_name: [
+          { required: true, message: `请输入参数名称`, trigger: 'blur' },
+          { min: 3, max: 8, message: '长度在 3 到 8 个', trigger: 'blur' },
+        ],
+      },
+      inputVisible: false,
+      inputValue: '',
     }
   },
   created() {
     this.catlist()
+    // this.lgindl()
   },
   computed: {
     isbled() {
@@ -124,14 +174,13 @@ export default {
     async catlist() {
       const res = await categorieslist()
       Object.keys(res).length == 1 ? this.$message.error(res.msg) : ''
-      //   console.log(res)
       this.parmlist = res.data
     },
     async handleChange() {
       this.parlists()
     },
     handleClick() {
-      console.log(this.activeName)
+      // console.log(this.activeName)
       this.parlists()
     },
     async parlists() {
@@ -141,7 +190,12 @@ export default {
       }
       const res = await categoriesacc(this.cateid, { params: { sel: this.activeName } })
       Object.keys(res).length == 1 ? this.$message.error(res.msg) : ''
-      console.log(res)
+      res.data.forEach(item => {
+        item.attr_vals = item.attr_vals ? item.attr_vals.split(' ') : []
+        this.inputVisible = false
+        this.inputValue = ''
+      })
+      console.log(res.data)
       if (this.activeName === 'many') {
         this.manytabledata = res.data
       } else {
@@ -163,8 +217,60 @@ export default {
         this.parlists()
       })
     },
+    addcscxg() {
+      this.$refs.addparsxgRef.resetFields()
+    },
+    async addpardd(id) {
+      // console.log(id)
+      this.xgparam = true
+      this.xgdata.id = id.cat_id
+      this.xgdata.attr_id = id.attr_id
+      const res = await putcates(id.cat_id, id.attr_id, {
+        params: { attr_sel: this.activeName },
+      })
+      // console.log(res)
+      this.addpresxg.attr_name = res.data.attr_name
+      this.addpresxg.attr_vals = res.data.attr_vals
+      this.addpresxg.attr_sel = res.data.attr_sel
+    },
+    async addparsmsxg() {
+      const res = await putcatesadd(this.xgdata.id, this.xgdata.attr_id, this.addpresxg)
+      Object.keys(res).length == 1 ? this.$message.error(res.msg) : this.$message.success(res.msg)
+      this.xgparam = false
+      this.parlists()
+    },
+    async removtpar(id) {
+      const res = await this.$confirm('需要删除账号吗？', '提示', {
+        confirmButtonText: '确认删除',
+        cancelButtonText: '取消删除',
+        type: 'warning',
+      }).catch(err => err)
+      if (res !== 'confirm') return this.$message.warning('取消删除成功')
+      const ress = await delcatesadd(id.cat_id, id.attr_id)
+      Object.keys(ress).length == 1 ? this.$message.error(ress.msg) : this.$message.success(ress.msg)
+      this.xgparam = false
+      this.parlists()
+    },
+    //keyup event native
+    handleInputConfirm() {},
+    showInput(id) {
+      console.log(id)
+      this.inputVisible = true
+      //获取input焦点
+      this.$nextTick(() => {
+        this.$refs.saveTagInput.$refs.input.focus()
+      })
+    },
   },
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.el-tag {
+  margin: 15px;
+}
+.input-new-tag {
+  width: 100px;
+  margin: 15px;
+}
+</style>
